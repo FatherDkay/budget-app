14e24dda42422f4a7e4edf4abead5338123641c5
FatherDkay <paul.d.bailey@hotmail.com>
2022-01-26 08:31:20 -0800

    moved idb to the correct folder

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,25 +1,33 @@
+// create variable to hold db connection
 let db;
-const request = indexedDB.open("budgettracker", 1);
+// establish a connection to IndexedDB database called 'budgetApp' and set it to version 1
+const request = indexedDB.open("budgetapp", 1);
 
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function(event) {
+    // save a reference to the database 
     const db = event.target.result;
+        // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore("new_transaction", { autoIncrement: true });
 };
 
+  // upon a successful 
+  request.onsuccess = function(event) {
+    db = event.target.result;
+};
+
+// upon an error
 request.onerror = function(event) {
     console.log(event.target.errorCode);
 };
 
-request.onsuccess = function(event) {
-    db = event.target.result;
-};
-
+// Save without internet connection
 function saveRecord(record) {
     const transaction = db.transaction(["new_transaction"], "readwrite");
     const budgetObjectStore = transaction.objectStore("new_transaction");
     budgetObjectStore.add(record);
 };
-
+// Upload when connected
 function uploadTransaction() {
     const transaction = db.transaction(["new_transaction"], "readwrite");
     const budgetObjectStore = transaction.objectStore("new_transaction");
