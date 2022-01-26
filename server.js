a585148c4ca2b22267876d151c206df4b6f67b20
FatherDkay <paul.d.bailey@hotmail.com>
2022-01-26 07:45:24 -0800

    idb complete

--- server.js
+++ server.js
@@ -9,11 +9,9 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
