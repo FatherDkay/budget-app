d17bd90be7c2c1a06b5869071f80c906ddb9d5e0
FatherDkay <paul.d.bailey@hotmail.com>
2022-01-26 09:24:44 -0800

    Deploy

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
